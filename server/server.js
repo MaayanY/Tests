b1f6e7beae2d124460baabdefc962bad8647ffb7
Maayan Yerushalmi <MaayanY@radware.com>
2017-06-11 05:53:19 +0300

    adding rabbit

--- server/server.js
+++ server/server.js
@@ -3,19 +3,46 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let amqp = require('amqp');
+
+let conOptions = {
+	host: 'localhost'
+};
+
+let rabbitMq = amqp.createConnection(conOptions);
+let rabbitExchange;
+
+
+
+rabbitMq.on('ready', () => {
+    rabbitExchange = rabbitMq.exchange('messages', {'type': 'fanout'});
+});
 
 io.on('connection', (socket) => {
-  console.log('user connected');
-  
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  
-  socket.on('add-message', (message) => {
-    socket.broadcast.emit('message', {type:'new-message', text: message});    
-  });
+	console.log('user connected');
+	rabbitMq.queue('messages-q',{exclusive: true}, function (q){
+		q.bind('messages', '');	
+		q.subscribe((message) => {
+		 	console.log(message);
+		 	socket.broadcast.emit('message', {type:'new-message', text: message.text}); 
+		 	
+		});
+	});
+	socket.on('add-message', (message) => {
+		
+		rabbitExchange.publish('', {text: message.toString()})
+		//socket.broadcast.emit('message', {type:'new-message', text: message});
+	});
+	socket.on('disconnect', function(){
+	    console.log('user disconnected');
+	});
+
 });
+
+
 
 http.listen(3000, () => {
   console.log('started on port 3000');
 });
+
+
